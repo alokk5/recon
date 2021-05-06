f9488ccbb379f5615b6c28d3e3fdc09331efc36a
alokk5 <alokk@yahoo.com>
2021-05-06 16:05:46 -0400

    Initial version

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'Hass33naahMaan$1234.5678.90$Ja0'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_DATABASE_URI = 'postgresql://reconuser1:reconuser1@localhost:5432/recon'
@@ -10,4 +11,6 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_USERNAME = 'alok1000@gmail.com'
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_PASSWORD = 'Gudiya1Darling'
