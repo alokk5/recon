be595f83e74285c9e1a95fa76e43f3d0f03d8a4e
alokk5 <alokk@yahoo.com>
2021-05-08 10:40:52 -0400

    Updated to mariadb

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = 'Hass33naahMaan$1234.5678.90$Ja0'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://reconuser1:reconuser1@localhost:5432/recon'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://recon_db_user:Ver$Go0d_99_Us3r@192.168.1.151/recon_dev_db?charset=utf8mb4'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
